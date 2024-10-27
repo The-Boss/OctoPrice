05a11927ab40c9613141213460faa7479acc9b7f
The-Boss <george.a.bostock@gmail.com>
2024-10-27 20:50:09 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello":"George"}
+
+@app.get("/price")
+async def read_root():
+    return {"colour":"red"}
+
